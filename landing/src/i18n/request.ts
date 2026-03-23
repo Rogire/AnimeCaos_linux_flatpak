6835039c6d998d9e693adc760d20e5c4769f67a2
caosdev <lanzonicmpny13@gmail.com>
2026-03-23 08:29:42 -0300

    new changes

--- landing/src/i18n/request.ts
+++
@@ -1,9 +0,0 @@
-import { getRequestConfig } from "next-intl/server";
-
-export default getRequestConfig(async ({ requestLocale }) => {
-    const locale = await requestLocale;
-    return {
-        locale: locale ?? "pt",
-        messages: (await import(`../messages/${locale ?? "pt"}.json`)).default,
-    };
-});
